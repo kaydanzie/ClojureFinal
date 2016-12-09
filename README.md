7ee341c92ac6e832f1ea387f6c5aae85115debc9
Kayla Ziegler <ziegl107@mail.chapman.edu>
2016-12-09 01:04:07 -0800

    added count, show-location. commented.

--- README.md
+++ README.md
@@ -2,6 +2,22 @@
 
 Clojure implementation of http://www.lisperati.com/casting.html
 
+##Language Concepts
+<ul>
+<li>Anonymous Functions</li>
+<li>Macros + Reader Macros</li>
+<li>Maps</li>
+<li>Symbols + Special Forms</li>
+<li>Immutability</li>
+</ul>
+
+##Added Functionality
+<ul>
+<li>Drop: used to drop currently-held objects in current location</li>
+<li>Count: tracks number of times objects have been picked up</li>
+<li>Show Locations: prints where all 4 objects are currently located</li>
+</ul>
+
 <!-- ## Usage
 
 FIXME
--- src/final/core.clj
+++ src/final/core.clj
@@ -9,12 +9,20 @@
    'garden '("you are in a beautiful garden. there is a well in front of you. "
              (east door living-room))
    'attic '("you are in the attic of the wizards house. there is a giant welding torch in the corner. "
-            (downstairs stairway living-room))})
+            (downstairs stairway living-room))
+   })
 
 (def obj-locations {'whiskey-bottle 'living-room
                     'bucket 'living-room
                     'chain 'garden
-                    'frog 'garden})
+                    'frog 'garden
+  })
+
+(def picked-up {'whiskey-bottle 0
+                'bucket 0
+                'chain 0
+                'frog 0
+  })
 
 (def location 'living-room)
 
@@ -22,6 +30,9 @@
 	(str (first (loc loc-map))))
 
 (defn describe-path [path]
+  ;second element in path is mode of transport, ex. door or staircase
+  ;first element is direction
+  ;defined in world map
   (str "there is a " (str (second path)) " going " (str (first path)) " from here. "))
 
 (defn describe-paths [location world]
@@ -30,16 +41,22 @@
   ;map applies describe-path to all elements of list
   (apply str (map describe-path (rest (get world location)))))
 
+;checks locations map for key value pair object-obj location
 (defn is-at? [object object-location locations]
   (= (object locations) object-location))
 
 (defn describe-floor [obj-location objects all-locations]
+  ;apply string to return of map
+  ;map applies anonymous function to return of filter
+  ;filter returns only objects that is-at? current location
+  ;can be more than one string
   (apply str (map (fn [x] 
                     (str "you see a " (str x) " on the floor. "))
                 (filter (fn [x] (is-at? x obj-location all-locations))
                       objects))))
 
 (defn look []
+  ;concatenate return strings of 3 functions
   (str (describe-loc location world)
     (describe-paths location world)
     (describe-floor location objects obj-locations)))
@@ -59,6 +76,10 @@
                 (look))
           :else "you can't go that way.")))
 
+;easier way to call functions
+;telling it to put quote in front of passed argument
+;quote = don't evaluate, return data structure
+;saying it's not a function call
 (defmacro defspel [& other] `(defmacro ~@other))
 
 (defspel walk [direction]
@@ -67,9 +88,12 @@
 (defn pickup-object [object]
   (cond (is-at? object location obj-locations)
             (do
-              ;return obj-locations with key-value pair (object 'body) added
+              ;redefine obj-locations with key-value pair (object 'body) added
               (def obj-locations (assoc obj-locations object 'body))
+              ;increase count associated with object picked up
+              (def picked-up (assoc picked-up object (inc (get picked-up object))))
               (str "you are now carrying the " (str object) "."))
+        ;if location of object being picked up is body
         (is-at? object 'body obj-locations)
             (str "you're already carrying the " (str object) ".")
         :else "you can't get that."))
@@ -77,22 +101,40 @@
 (defspel pickup [object]
   `(pickup-object '~object))
 
+;print number of times item picked up using picked-up map
+(defn get-count [object]
+  (str "you've picked up the " (str object) " " (str (get picked-up object)) " time(s)."))
+
 (defn inventory [] 
+  ;filter objects in obj-locations based on where they are
+  ;return objects where location = body
   (filter (fn[x] (is-at? x 'body obj-locations))
           objects))
 
 (defn is-holding? [object]
   (cond
+    ;some #{object} returns value that is in inventory
+    ;return false if nothing returns
     (nil? (some #{object} (inventory)))
     false
     :else true))
 
+;remove object from inventory (body)
+;leave in current location, update location in obj-locations
 (defn drop-object [object]
   (cond (is-holding? object)
     (do
       (def obj-locations (assoc obj-locations object location))
       (str "you dropped the " (str object) " in the " (str location) "."))
     :else (str "you are not holding the " (str object) ".")))
+
+(defn show-locations [objects]
+  (apply str (map (fn [x] 
+                    (cond (not= (get obj-locations x) 'body)
+                      (str "the " (str x) " is in the " (str (get obj-locations x)) ". ")
+                      :else (str "you are holding the " (str x) ". "))
+                    )
+                objects)))
 
 (def chain-welded false)
 (def bucket-filled false)
@@ -109,6 +151,8 @@
 
 (game-action weld chain bucket attic
   (cond
+    ;game-action macro only checks is-holding? for one object
+    ;need to confirm is-holding? chain AND bucket
     (and (is-holding? 'bucket)
       (def chain-welded true))
       "the chain is now securely welded to the bucket."
@@ -144,7 +188,13 @@
   (newline)
   (println "PICKUP: " (pickup chain))
   (newline)
+  (println "WALK: " (walk east))
+  (newline)
   (println "DROP: " (drop-object 'frog))
+  (newline)
+  (println (walk west))
+  (newline)
+  (println "PICKUP: " (pickup frog))
   (newline)
   (println "WALK: " (walk east))
   (newline)
@@ -158,7 +208,11 @@
   (newline)
   (println "DUNK: " (dunk bucket well))
   (newline)
+  (println "COUNT: " (get-count 'frog))
+  (newline)
   (println "WALK: " (walk east))
+  (newline)
+  (println (show-locations objects))
   (newline)
   (println (splash bucket wizard))
   )
